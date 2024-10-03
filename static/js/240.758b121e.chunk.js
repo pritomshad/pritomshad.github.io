5e9d56787be2eafdae0eaedf8f76b7adbc7f65e1
prit0m <59039346+pritomshad@users.noreply.github.com>
2024-10-04 00:02:50 +0600

    Add files via upload

---
+++ static/js/240.758b121e.chunk.js
@@ -0,0 +1,74 @@
+"use strict";(self.webpackChunkmy_app=self.webpackChunkmy_app||[]).push([[240],{389:(e,t,n)=>{n.d(t,{A:()=>d});var r=n(9281),i=n(9);const a=(0,i.Ay)("p")`
+  margin-top: 1.5rem;
+`,o=(0,i.Ay)("div")`
+  position: relative;
+  max-width: 700px;
+`,s=(0,i.Ay)("div")`
+  border-radius: 3rem;
+  max-width: 400px;
+`;var l=n(579);const d=(0,r.C)()((e=>{let{title:t,content:n,t:r}=e;return(0,l.jsxs)(o,{children:[(0,l.jsx)("h6",{children:r(t)}),(0,l.jsx)(s,{children:(0,l.jsx)(a,{children:r(n)})})]})}))},3240:(e,t,n)=>{n.r(t),n.d(t,{default:()=>k});var r=n(5043);const i=JSON.parse('{"DD":"Our Hospital Helps people","Qq":"We provide compassionate maternity care for mothers and newborns.","x6":[{"title":"Explore"},{"title":"Learn more","color":"#fff"}]}'),a=JSON.parse('{"D":"Our Mission","Q":"Our mission is to offer exceptional maternal and newborn care, ensuring a safe and supportive environment for every family. We prioritize compassionate, personalized healthcare, promoting the well-being of mothers and babies through expert medical services, education, and holistic support throughout pregnancy and beyond."}'),o=JSON.parse('{"D":"Contact form","Q":"The following form demonstrates form validation in action. Contact form component reduces the amount of time it is being re-rendered by the user as it embraces uncontrolled form validation to reduce any unnecessary performance penalty."}');var s=n(1319),l=n(4647),d=n(9);const c=(0,d.Ay)("div")`
+  margin-top: 5rem;
+  padding: 5rem 0;
+
+  @media only screen and (max-width: 1024px) {
+    padding: 3rem 0;
+  }
+`,p=((0,d.Ay)("div")`
+  margin-left: 0rem;
+`,(0,d.Ay)("a")`
+    color: rgb(56, 192, 255);
+    text-underline-position: under;
+    text-decoration: rgb(56, 192, 255) wavy underline;
+`,(0,d.Ay)("div")`
+  border-style: solid;
+  border-color: #4d6478;
+  background-image: url('/img/svg/card-back-1.png');
+  aspect-ratio: 3 / 4;
+  border-radius: 12px;
+  width: 320px;
+  position: relative;
+  grid-template-rows: 1fr auto;
+  box-shadow: 0 1rem 2rem -1rem black;
+  padding: 1rem;
+  margin-left: 6rem;
+
+  @media only screen and (max-width: 945px) {
+    margin: 1.5rem;
+  }
+
+  @media only screen and (max-width: 414px) {
+    margin: 1.5rem;
+  }
+  
+  @media only screen and (max-width: 375px) {
+    margin: 0.5rem;
+  }
+
+`),m=(0,d.Ay)("div")`
+  border-style: solid;
+  border-color: #4d6478;
+  background-image: url('/img/svg/card-back-2.png');
+  aspect-ratio: 3 / 4;
+  border-radius: 12px;
+  width: 320px;
+  position: relative;
+  grid-template-rows: auto 1fr;
+  box-shadow: 0 1rem 2rem -1rem black;
+  padding: 1rem;
+  display: grid;
+  margin-left: 6rem;
+  
+  @media only screen and (max-width: 945px) {
+    margin: 1.5rem;
+  }
+  
+  @media only screen and (max-width: 414px) {
+    margin: 1.5rem;
+  }
+
+  @media only screen and (max-width: 375px) {
+    margin: 0.5rem;
+  }
+
+`;n(389);var x=n(9281),u=n(579);const y=(0,x.C)()((e=>{let{title:t,content:n,id:r}=e;return(0,u.jsxs)(c,{id:r,children:[(0,u.jsxs)(s.A,{justify:"space-between",align:"top",children:[(0,u.jsx)(l.A,{span:8,children:" "}),(0,u.jsx)(l.A,{span:8,children:(0,u.jsx)("h1",{style:{textAlign:"center"},children:"Donate Here"})}),(0,u.jsx)(l.A,{span:8})]}),(0,u.jsxs)(s.A,{justify:"space-between",align:"middle",style:{textAlign:"center"},children:[(0,u.jsx)(l.A,{span:12,lg:12,md:12,sm:24,xs:24,children:(0,u.jsxs)(p,{children:[(0,u.jsx)("p",{style:{textAlign:"center"},children:"One-time Donation"}),(0,u.jsx)("stripe-buy-button",{"buy-button-id":"buy_btn_1PwIQYRxzF3XYr6InCstLarI","publishable-key":"pk_test_51PrF6aRxzF3XYr6I539lJURyZDp1NYOcQvxicld10KckilXDjFHxunR5ifOGXVPgVz1ajDC4M3PlRIupF8CkyCOk00K8yE9Mgn"})]})}),(0,u.jsx)(l.A,{span:12,lg:12,md:12,sm:24,xs:24,children:(0,u.jsxs)(m,{children:[(0,u.jsx)("p",{style:{textAlign:"center"},children:"Monthly Donation"}),"If you want to support our cause monthly, please select any of the options below.",(0,u.jsx)("stripe-buy-button",{"buy-button-id":"buy_btn_1Q5shkRxzF3XYr6IwrQN4awA","publishable-key":"pk_test_51PrF6aRxzF3XYr6I539lJURyZDp1NYOcQvxicld10KckilXDjFHxunR5ifOGXVPgVz1ajDC4M3PlRIupF8CkyCOk00K8yE9Mgn"}),(0,u.jsx)("stripe-buy-button",{"buy-button-id":"buy_btn_1Q5szbRxzF3XYr6IcvRMnvXd","publishable-key":"pk_test_51PrF6aRxzF3XYr6I539lJURyZDp1NYOcQvxicld10KckilXDjFHxunR5ifOGXVPgVz1ajDC4M3PlRIupF8CkyCOk00K8yE9Mgn"}),(0,u.jsx)("stripe-buy-button",{"buy-button-id":"buy_btn_1Q5t1iRxzF3XYr6IZ3tcpnBw","publishable-key":"pk_test_51PrF6aRxzF3XYr6I539lJURyZDp1NYOcQvxicld10KckilXDjFHxunR5ifOGXVPgVz1ajDC4M3PlRIupF8CkyCOk00K8yE9Mgn"}),(0,u.jsx)("stripe-buy-button",{"buy-button-id":"buy_btn_1Q5t2gRxzF3XYr6ICAANqUBc","publishable-key":"pk_test_51PrF6aRxzF3XYr6I539lJURyZDp1NYOcQvxicld10KckilXDjFHxunR5ifOGXVPgVz1ajDC4M3PlRIupF8CkyCOk00K8yE9Mgn"})]})})]})]})})),b=(0,r.lazy)((()=>Promise.all([n.e(902),n.e(310)]).then(n.bind(n,3310)))),h=(0,r.lazy)((()=>Promise.resolve().then(n.bind(n,1766)))),g=(0,r.lazy)((()=>n.e(538).then(n.bind(n,8538)))),j=(0,r.lazy)((()=>Promise.all([n.e(902),n.e(566)]).then(n.bind(n,6566)))),k=()=>(0,u.jsxs)(h,{children:[(0,u.jsx)(j,{direction:"right",title:i.DD,content:i.Qq,button:i.x6,icon:"1.jpg",id:"intro"}),(0,u.jsx)(j,{direction:"right",title:a.D,content:a.Q,icon:"care.svg",id:"mission"}),(0,u.jsx)(b,{title:o.D,content:o.Q,id:"contact"}),(0,u.jsx)(y,{title:"Donate Here",content:"Your Donation will bring smile to people",id:"donate"}),(0,u.jsx)(g,{})]})}}]);
+//# sourceMappingURL=240.758b121e.chunk.js.map
