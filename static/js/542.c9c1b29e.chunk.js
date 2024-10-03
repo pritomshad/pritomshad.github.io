5aa67186f8a465d8cde7f0ba7c70026c4f817e5f
prit0m <59039346+pritomshad@users.noreply.github.com>
2024-10-04 00:17:00 +0600

    Add files via upload

---
+++ static/js/542.c9c1b29e.chunk.js
@@ -0,0 +1,50 @@
+"use strict";(self.webpackChunkmy_app=self.webpackChunkmy_app||[]).push([[542],{1058:(e,i,a)=>{a.r(i),a.d(i,{default:()=>h});var t=a(5043),n=a(2382),l=(a(4430),a(9078),a(9));(0,l.Ay)("div")`
+  display: 'flex';
+  alignItems: 'center';
+  justifyContent: 'center';
+  height: 500px;
+`;const d=(0,l.Ay)("img")`
+  display: 'flex';
+  alignItems: 'center';
+  justifyContent: 'center';
+  max-width: calc(100% - 80px);
+  max-height: 800px;
+  margin-right: auto;
+  margin-left: auto;
+  padding-left: 15%;
+  padding-right: 15%;
+
+  @media only screen and (max-width: 1024px) {
+    max-width: calc(100% - 68px);
+    padding: 0 30px;
+  }
+
+  @media only screen and (max-width: 768px) {
+    max-width: calc(100% - 38px);
+    padding: 0 18px;
+  }
+
+  @media only screen and (max-width: 414px) {
+    max-width: 100%;
+    padding: 0 18px;
+  }
+`,r=(0,l.Ay)("div")`
+  padding: 3rem 0;
+  text-align: center;
+
+  @media only screen and (max-width: 1024px) {
+    padding: 3rem 0;
+  }
+`;var s=a(579);const o=["/img/gallery/1.jpg","/img/gallery/2.jpg","/img/gallery/3.jpg","/img/gallery/4.jpg","/img/gallery/5.jpg"],p=e=>{let{id:i}=e;return(0,s.jsxs)(r,{id:i,children:[(0,s.jsx)("h2",{color:"white",children:"Gallery"}),(0,s.jsx)(n.A,{dots:!0,speed:500,slidesToShow:1,slidesToScroll:1,infinite:!0,autoplay:!0,autoplaySpeed:5e3,children:o.map(((e,i)=>(0,s.jsx)(d,{src:e,alt:"hospital photo"})))})]})};var c=a(1688);const g=(0,t.lazy)((()=>a.e(538).then(a.bind(a,8538)))),m=(0,t.lazy)((()=>Promise.resolve().then(a.bind(a,1766)))),x=(0,l.Ay)("a")`
+    color: #18456a;
+    font-size: 1rem;
+
+    &:hover,
+    &:active,
+    &:focus {
+        color: rgb(56, 192, 255);
+        text-underline-position: under;
+        text-decoration: rgb(56, 192, 255) wavy underline;
+    }
+`,h=()=>{const e=(0,c.W6)();return(0,s.jsxs)(m,{children:[(0,s.jsxs)(x,{id:"intro",onClick:()=>{e.push("/home")},children:[" ",(0,s.jsx)("img",{src:"/img/svg/back.png",alt:"left-arrow",width:"20rem"})," Go to Home"]}),(0,s.jsx)(p,{id:"photoGallery"}),(0,s.jsx)(g,{})]})}}}]);
+//# sourceMappingURL=542.c9c1b29e.chunk.js.map
