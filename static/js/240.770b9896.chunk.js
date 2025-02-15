43842913b77c7f0a20117db3e3c2733bef8a39b2
pritomshad <pritomd678@gmail.com>
2025-02-15 16:08:19 +0600

    update website

---
+++ static/js/240.770b9896.chunk.js
@@ -0,0 +1,74 @@
+"use strict";(self.webpackChunkmy_app=self.webpackChunkmy_app||[]).push([[240],{389:(e,t,n)=>{n.d(t,{A:()=>d});var i=n(9281),r=n(9);const a=(0,r.Ay)("p")`
+  margin-top: 1.5rem;
+`,s=(0,r.Ay)("div")`
+  position: relative;
+  max-width: 700px;
+`,o=(0,r.Ay)("div")`
+  border-radius: 3rem;
+  max-width: 400px;
+`;var l=n(579);const d=(0,i.C)()((e=>{let{title:t,content:n,t:i}=e;return(0,l.jsxs)(s,{children:[(0,l.jsx)("h6",{children:i(t)}),(0,l.jsx)(o,{children:(0,l.jsx)(a,{children:i(n)})})]})}))},3240:(e,t,n)=>{n.r(t),n.d(t,{default:()=>k});var i=n(5043);const r=JSON.parse('{"DD":"Our Hospital Helps people","Qq":"We provide compassionate maternity care for mothers and newborns.","x6":[{"title":"Explore"},{"title":"Learn more","color":"#fff"}]}'),a=JSON.parse('{"D":"Our Mission","Q":"Our mission is to offer exceptional maternal and newborn care, ensuring a safe and supportive environment for every family. We prioritize compassionate, personalized healthcare, promoting the well-being of mothers and babies through expert medical services, education, and holistic support throughout pregnancy and beyond."}'),s=JSON.parse('{"D":"Connect With Us","Q":""}');var o=n(1319),l=n(4647),d=n(9);const p=(0,d.Ay)("div")`
+  margin-top: 5rem;
+  padding: 5rem 0;
+
+  @media only screen and (max-width: 1024px) {
+    padding: 3rem 0;
+  }
+`,c=((0,d.Ay)("div")`
+  margin-left: 0rem;
+`,(0,d.Ay)("a")`
+    color: rgb(56, 192, 255);
+    text-underline-position: under;
+    text-decoration: rgb(56, 192, 255) wavy underline;
+`,(0,d.Ay)("div")`
+  border-style: solid;
+  border-color: #4d6478;
+  background-image: url('/img/svg/card-back-1.png');
+  aspect-ratio: 3 / 4;
+  border-radius: 12px;
+  width: 320px;
+  position: relative;
+  grid-template-rows: 1fr auto;
+  box-shadow: 0 1rem 2rem -1rem black;
+  padding: 1rem;
+  margin-left: 6rem;
+
+  @media only screen and (max-width: 945px) {
+    margin: 1.5rem;
+  }
+
+  @media only screen and (max-width: 414px) {
+    margin: 1.5rem;
+  }
+  
+  @media only screen and (max-width: 375px) {
+    margin: 0.5rem;
+  }
+
+`),x=(0,d.Ay)("div")`
+  border-style: solid;
+  border-color: #4d6478;
+  background-image: url('/img/svg/card-back-2.png');
+  aspect-ratio: 3 / 4;
+  border-radius: 12px;
+  width: 320px;
+  position: relative;
+  grid-template-rows: auto 1fr;
+  box-shadow: 0 1rem 2rem -1rem black;
+  padding: 1rem;
+  display: grid;
+  margin-left: 6rem;
+  
+  @media only screen and (max-width: 945px) {
+    margin: 1.5rem;
+  }
+  
+  @media only screen and (max-width: 414px) {
+    margin: 1.5rem;
+  }
+
+  @media only screen and (max-width: 375px) {
+    margin: 0.5rem;
+  }
+
+`;n(389);var m=n(9281),u=n(579);const b=(0,m.C)()((e=>{let{title:t,content:n,id:i}=e;return(0,u.jsxs)(p,{id:i,children:[(0,u.jsxs)(o.A,{justify:"space-between",align:"top",children:[(0,u.jsx)(l.A,{span:8,children:" "}),(0,u.jsx)(l.A,{span:8,children:(0,u.jsx)("h1",{style:{textAlign:"center"},children:"Donate Here"})}),(0,u.jsx)(l.A,{span:8})]}),(0,u.jsxs)(o.A,{justify:"space-between",align:"middle",style:{textAlign:"center"},children:[(0,u.jsx)(l.A,{span:12,lg:12,md:12,sm:24,xs:24,children:(0,u.jsxs)(c,{children:[(0,u.jsx)("p",{style:{textAlign:"center"},children:"One-time Donation"}),(0,u.jsx)("stripe-buy-button",{"buy-button-id":"buy_btn_1PwIQYRxzF3XYr6InCstLarI","publishable-key":"pk_test_51PrF6aRxzF3XYr6I539lJURyZDp1NYOcQvxicld10KckilXDjFHxunR5ifOGXVPgVz1ajDC4M3PlRIupF8CkyCOk00K8yE9Mgn"})]})}),(0,u.jsx)(l.A,{span:12,lg:12,md:12,sm:24,xs:24,children:(0,u.jsxs)(x,{children:[(0,u.jsx)("p",{style:{textAlign:"center"},children:"Monthly Donation"}),"If you want to support our cause monthly, please select any of the options below.",(0,u.jsx)("stripe-buy-button",{"buy-button-id":"buy_btn_1Q5shkRxzF3XYr6IwrQN4awA","publishable-key":"pk_test_51PrF6aRxzF3XYr6I539lJURyZDp1NYOcQvxicld10KckilXDjFHxunR5ifOGXVPgVz1ajDC4M3PlRIupF8CkyCOk00K8yE9Mgn"}),(0,u.jsx)("stripe-buy-button",{"buy-button-id":"buy_btn_1Q5szbRxzF3XYr6IcvRMnvXd","publishable-key":"pk_test_51PrF6aRxzF3XYr6I539lJURyZDp1NYOcQvxicld10KckilXDjFHxunR5ifOGXVPgVz1ajDC4M3PlRIupF8CkyCOk00K8yE9Mgn"}),(0,u.jsx)("stripe-buy-button",{"buy-button-id":"buy_btn_1Q5t1iRxzF3XYr6IZ3tcpnBw","publishable-key":"pk_test_51PrF6aRxzF3XYr6I539lJURyZDp1NYOcQvxicld10KckilXDjFHxunR5ifOGXVPgVz1ajDC4M3PlRIupF8CkyCOk00K8yE9Mgn"}),(0,u.jsx)("stripe-buy-button",{"buy-button-id":"buy_btn_1Q5t2gRxzF3XYr6ICAANqUBc","publishable-key":"pk_test_51PrF6aRxzF3XYr6I539lJURyZDp1NYOcQvxicld10KckilXDjFHxunR5ifOGXVPgVz1ajDC4M3PlRIupF8CkyCOk00K8yE9Mgn"})]})})]})]})})),y=(0,i.lazy)((()=>Promise.all([n.e(197),n.e(297),n.e(310)]).then(n.bind(n,3310)))),h=(0,i.lazy)((()=>Promise.resolve().then(n.bind(n,1766)))),g=(0,i.lazy)((()=>n.e(538).then(n.bind(n,8538)))),j=(0,i.lazy)((()=>Promise.all([n.e(197),n.e(566)]).then(n.bind(n,6566)))),k=()=>(0,u.jsxs)(h,{children:[(0,u.jsx)(j,{direction:"right",title:r.DD,content:r.Qq,button:r.x6,icon:"1.jpg",id:"intro"}),(0,u.jsx)(j,{direction:"right",title:a.D,content:a.Q,icon:"care.svg",id:"mission"}),(0,u.jsx)(y,{title:s.D,content:s.Q,id:"contact"}),(0,u.jsx)(b,{title:"Donate Here",content:"Your Donation will bring smile to people",id:"donate"}),(0,u.jsx)(g,{})]})}}]);
+//# sourceMappingURL=240.770b9896.chunk.js.map
---
+++ static/js/310.30244ac7.chunk.js
@@ -0,0 +1,36 @@
+"use strict";(self.webpackChunkmy_app=self.webpackChunkmy_app||[]).push([[310],{3310:(e,i,r)=>{r.r(i),r.d(i,{default:()=>j});var n=r(1319),l=r(4647),t=r(9281),s=r(9197),o=r(389),a=r(9);const c=(0,a.Ay)("div")`
+  padding: 5rem 0;
+
+  @media only screen and (max-width: 1024px) {
+    padding: 3rem 0;
+  }
+`,d=((0,a.Ay)("form")`
+  width: 100%;
+  max-width: 520px;
+
+  @media only screen and (max-width: 1045px) {
+    max-width: 100%;
+    margin-top: 2rem;
+  }
+`,(0,a.Ay)("span")`
+  display: block;
+  font-weight: 600;
+  color: rgb(255, 130, 92);
+  height: 0.775rem;
+  padding: 0 0.675rem;
+`,(0,a.Ay)("div")`
+  text-align: end;
+  position: relative;
+
+  @media only screen and (max-width: 414px) {
+    padding-top: 0.75rem;
+  }
+`,(0,a.Ay)("ul")`
+  display: flex;
+  align-items: center;
+  gap: 1rem;
+  list-style-type: none;
+  padding: 0;
+  margin: 0;
+`);var x=r(3910),h=r(892),m=r(7929),p=r(579);const g=()=>(0,p.jsxs)("div",{children:[(0,p.jsxs)(d,{children:[(0,p.jsx)("li",{style:{marginBottom:"12px"},children:(0,p.jsx)(x.g,{icon:m.y_8,size:"2x"})}),(0,p.jsx)("li",{children:(0,p.jsx)("a",{href:"mailto: mnmhospital0@gmail.com",style:{textDecoration:"none",color:"black"},children:(0,p.jsx)("p",{children:"mnmhospital0@gmail.com"})})})]}),(0,p.jsxs)(d,{children:[(0,p.jsx)("li",{style:{marginBottom:"12px"},children:(0,p.jsx)(x.g,{icon:m.ggt,size:"2x"})}),(0,p.jsx)("a",{href:"tel:+447916678918",style:{textDecoration:"none",color:"black"},children:(0,p.jsx)("p",{children:"+44 7916 678918"})})]}),(0,p.jsxs)(d,{children:[(0,p.jsx)("li",{style:{marginBottom:"12px"},children:(0,p.jsx)(x.g,{icon:m.KKr,size:"2x"})}),(0,p.jsx)("a",{href:"tel:07342692934",style:{textDecoration:"none",color:"black"},children:(0,p.jsx)("p",{children:"07342692934"})})]})]}),j=(0,t.C)()((e=>{let{title:i,content:r,id:t,t:a}=e;return(0,p.jsx)(c,{id:t,children:(0,p.jsxs)(n.A,{justify:"space-between",align:"middle",children:[(0,p.jsx)(l.A,{lg:12,md:11,sm:24,xs:24,children:(0,p.jsxs)(s.q7,{direction:"left",triggerOnce:!0,children:[(0,p.jsx)(o.A,{title:i,content:r}),(0,p.jsx)(g,{})]})}),(0,p.jsx)(l.A,{lg:12,md:12,sm:24,xs:24,children:(0,p.jsxs)(s.q7,{direction:"right",triggerOnce:!0,children:[(0,p.jsxs)(d,{children:[(0,p.jsx)("li",{style:{marginBottom:"3px"},children:(0,p.jsx)("a",{href:"https://www.facebook.com/profile.php?id=61564927004198",target:"_blank",rel:"noopener noreferrer",children:(0,p.jsx)(x.g,{icon:h.aUl,size:"2x",style:{color:"#1877F2"}})})}),(0,p.jsx)("li",{children:(0,p.jsx)("a",{href:"https://www.facebook.com/profile.php?id=61564927004198",target:"_blank",rel:"noopener noreferrer",children:(0,p.jsx)("p",{style:{marginBottom:"3px"},children:"/MNM Hospital"})})})]}),(0,p.jsxs)(d,{children:[(0,p.jsx)("li",{children:(0,p.jsx)("a",{href:"https://www.instagram.com/mnmhospital/",target:"_blank",rel:"noopener noreferrer",children:(0,p.jsx)(x.g,{icon:h.QV6,size:"2x",style:{color:"#E4405F"}})})}),(0,p.jsx)("li",{children:(0,p.jsx)("a",{href:"https://www.instagram.com/mnmhospital/",target:"_blank",rel:"noopener noreferrer",children:(0,p.jsx)("p",{style:{marginBottom:"3px"},children:"/mnmhospital"})})})]})]})})]})})}))}}]);
+//# sourceMappingURL=310.30244ac7.chunk.js.map
